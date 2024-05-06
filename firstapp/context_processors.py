88259d54272f17063b54eb9ecbd2b132078297eb
Garth Mortensen <mortensengarth@hotmail.com>
2024-05-05 19:42:34 -0500

    Django: Add context processor to make dictionary universally accessible

---
+++ firstapp/context_processors.py
@@ -0,0 +1,5 @@
+# To make a variable available to all views, use Django's context processors.
+# Context processor = function that takes the request object, returns a dict
+# this dict is added to the context
+def website_name(request):
+    return {"website_name": "supercool"}
