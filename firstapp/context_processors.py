1a44b8cf3371b632bdf19ebf7c48f247e91c690c
Garth Mortensen <mortensengarth@hotmail.com>
2024-05-05 19:54:43 -0500

    Code: Make html block title clearer to understand

--- firstapp/context_processors.py
+++ firstapp/context_processors.py
@@ -2,4 +2,4 @@
 # Context processor = function that takes the request object, returns a dict
 # this dict is added to the context
 def website_name(request):
-    return {"website_name": "supercool"}
+    return {"website_name": "supercool",}
--- firstapp/views.py
+++ firstapp/views.py
@@ -1,27 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def home(request):
     context = {
-        'page_name': 'home',
-        'message': 'Hello, Mars!',
+        "page_name": "home",
+        "message": "Hello, Mars!",
     }
-    return render(request, 'firstapp/home.html', context)
+    return render(request, "firstapp/home.html", context)
+
 
 def contact(request):
     context = {
-        'page_name': 'contact',
+        "page_name": "contact",
     }
-    return render(request, 'firstapp/contact.html', context)
+    return render(request, "firstapp/contact.html", context)
+
 
 def portfolio(request):
     context = {
-        'page_name': 'portfolio',
+        "page_name": "portfolio",
     }
-    return render(request, 'firstapp/portfolio.html', context)
+    return render(request, "firstapp/portfolio.html", context)
+
 
 def statement(request):
     context = {
-        'page_name': 'statement',
+        "page_name": "statement",
     }
-    return render(request, 'firstapp/statement.html', context)
+    return render(request, "firstapp/statement.html", context)
