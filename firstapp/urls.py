9fa29fbc9bd5d635ef48eb4d3368c5ed10f48a2c
Garth Mortensen <mortensengarth@hotmail.com>
2024-05-18 06:54:44 -0500

    Feature: Replace portfolio with dropdown and subpages

--- firstapp/urls.py
+++ firstapp/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
-from .views import home, contact, portfolio, statement
+from .views import home, contact, landscapes, japanese_koi_fish, food, wooden_kokeshi_dolls, teapots, statement
 
 # create a list of URL patterns
 urlpatterns = [
-    path("", home, name="home"),
-    path('contact/', contact, name='contact'),
-    path('portfolio/', portfolio, name='portfolio'),
-    path('statement/', statement, name='statement'),
+    path(route="", view=home, name="home"),
+    path(route='contact/', view=contact, name='contact'),
+    path(route='landscapes/', view=landscapes, name='landscapes'),
+    path(route='japanese_koi_fish/', view=japanese_koi_fish, name='japanese_koi_fish'),
+    path(route='food/', view=food, name='food'),
+    path(route='wooden_kokeshi_dolls/', view=wooden_kokeshi_dolls, name='wooden_kokeshi_dolls'),
+    path(route='teapots/', view=teapots, name='teapots'),
+    path(route='statement/', view=statement, name='statement'),
 ]
--- firstapp/views.py
+++ firstapp/views.py
@@ -17,11 +17,25 @@ def contact(request):
     return render(request, "firstapp/contact.html", context)
 
 
-def portfolio(request):
-    context = {
-        "page_name": "portfolio",
-    }
-    return render(request, "firstapp/portfolio.html", context)
+def landscapes(request):
+    context = {'title': 'Landscapes'}
+    return render(request, 'firstapp/portfolio/landscapes.html', context)
+
+def japanese_koi_fish(request):
+    context = {'title': 'Japanese Koi Fish'}
+    return render(request, 'firstapp/portfolio/japanese_koi_fish.html', context)
+
+def food(request):
+    context = {'title': 'Food'}
+    return render(request, 'firstapp/portfolio/food.html', context)
+
+def wooden_kokeshi_dolls(request):
+    context = {'title': 'Wooden Kokeshi Dolls'}
+    return render(request, 'firstapp/portfolio/wooden_kokeshi_dolls.html', context)
+
+def teapots(request):
+    context = {'title': 'Teapots'}
+    return render(request, 'firstapp/portfolio/teapots.html', context)
 
 
 def statement(request):
