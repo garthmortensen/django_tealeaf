664cc86d19650f3f6e2f482edc2fbf273aff6f58
Garth Mortensen <mortensengarth@hotmail.com>
2024-05-19 07:32:46 -0500

    Feature: Add Media folder categorization into subfolders

--- firstapp/models.py
+++ firstapp/models.py
@@ -7,14 +7,18 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+# this generates the upload path for the image field
+def get_upload_path(instance, filename):
+    return 'paintings/{}/{}'.format(instance.category.name, filename)
 
+# Painting model has FK to the Category model
 class Painting(models.Model):
     category = models.ForeignKey(
         Category, related_name="paintings", on_delete=models.CASCADE
     )
     title = models.CharField(max_length=200)
     description = models.TextField()
-    image = models.ImageField(upload_to="paintings/")
+    image = models.ImageField(upload_to=get_upload_path)
 
     def __str__(self):
         return self.title
---
+++ media/paintings/README.md
@@ -0,0 +1,1 @@
+Note - create in admin page categories, and then upload a new image to that category. The category folders will be automatically created if they dont exist, and image will be placed inside.
