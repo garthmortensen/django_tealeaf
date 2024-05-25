ef1c899ffe167f772b0153e8e4c73326f0c5893f
Garth Mortensen <mortensengarth@hotmail.com>
2024-05-24 22:31:06 -0500

    Style: Fix header padding, play with Productionalization

--- tealeafdjangoproj/settings.py
+++ tealeafdjangoproj/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-ykl1%ma4iukpl)nur8u!1nt*e!tf!*v!-e8r51u4m*$-wzlxil"
+SECRET_KEY = "$GrsgrsfES$%TRSGkyyGRDykl1%ma4iukpl)nur8u!1nt*e!tf!*v!-e8r51u4m*$-wzlxil"
+# SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
